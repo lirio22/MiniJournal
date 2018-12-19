f249773622ff7a026d9a70915ef2488825f66140
lirio22 <maira_sayuri@hotmail.com>
2018-12-19 17:50:35 -0200

    Last Commit Failed

---
+++ Assets/_Scripts/MonthDayManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonthDayManager : MonoBehaviour {
+    public Image box;
+    public string journal = "";
+
+    public void Awake()
+    {
+        box = GetComponent<Image>();
+    }
+
+    public void SetDay(Color newColor, string newEntry)
+    {
+        box.color = newColor;
+        journal = newEntry;
+    }
+}
